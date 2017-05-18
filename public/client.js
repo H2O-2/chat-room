724a783752735e9405382ea054146961d7d32247
H2O-2 <1064737282@qq.com>
2017-05-18 08:47:47 -0400

    chat function

--- public/client.js
+++ public/client.js
@@ -1,65 +1,126 @@
-// obtain username from user.
-$('#username-form').modal();
-$('#username').focus();
+$(document).ready(function(){
+	// obtain username from user.
+	$('#username-form').modal();
+	$('#username').focus();
 
-// these are Jquery elements.
-var usernameText;
-var avatarSrc;
+	// these are Jquery elements.
+	var usernameText;
+	var avatarSrc;
 
-var username2jqy = function(usrString){
-	var username = $('<div></div>').addClass('username');
-	$('<span></span>').text(usrString).appendTo(username);
-	return username;
-};
-
-var avatar2jqy = function(avaString){
-	return $('<img></img>').attr('src', avaString);
-};
-
-var setNameNAvatar = function(){
-	usernameText = $('#username').val() ? $('#username').val() : 'Anonymous';
-	$.modal.close();
-	avatarSrc = 'http://api.adorable.io/avatar/80/' + usernameText;
-	$('#message-box').focus();
-};
-
-$('#username-btn').click(function(e){
-	setNameNAvatar();
-});
-
-$('#username-form').keydown(function(e){
-	if(!e){
-		e = window.event;
-	}
-	if(e.keyCode == 13){
-		setNameNAvatar();
-	}
-});
-	
-
-var socket = io();
-
-// send new message & clear input
-var sendMsg = function() {
-	var msgText = $('#message-box').val();
-
-	if (msgText.length === 0) return;
-
-	var msg = {
-		'username': usernameText,
-		'avatar': avatarSrc,
-		'msg': msgText
+	var username2jqy = function(usrString){
+		var username = $('<div></div>').addClass('username');
+		$('<span></span>').text(usrString).appendTo(username);
+		return username;
 	};
 
-	if (msg) socket.emit('message', msg, function(data) {
-		console.log(data);
-	});
-};
+	var avatar2jqy = function(avaString){
+		return $('<img></img>').attr('src', avaString);
+	};
 
-socket.on('connect', function() {
-	$('#message-btn').click(function(e) {
-		e.preventDefault();
-		sendMsg();
+	var setNameNAvatar = function(){
+		var fakeEmail = 'YourEmail@email.com';
+
+		usernameText = $('#username').val() ? $('#username').val() : 'Anonymous';
+
+		$.modal.close();
+		avatarSrc = 'https://www.gravatar.com/avatar/' + md5(fakeEmail.toLowerCase());
+		$('#message-box').focus();
+		socket.emit('createUser', {
+			'username': usernameText,
+			'avatar': avatarSrc
+		});
+	};
+
+	$('#username-btn').click(function(e){
+		console.log('clicked');
+		setNameNAvatar();
+	});
+
+	$('#username-form').keydown(function(e){
+		if(!e){
+			e = window.event;
+		}
+		if(e.keyCode == 13){
+			setNameNAvatar();
+		}
+	});
+		
+
+	var socket = io();
+
+	// send new message & clear input
+	var sendMsg = function() {
+		var msgText = $('#message-box').val();
+
+		if (msgText.length === 0) return;
+
+		var msg = {
+			'username': usernameText,
+			'avatar': avatarSrc,
+			'msg': msgText
+		};
+
+		//console.log(usernameText);
+
+		if (msg) socket.emit('message', msg, function(data) {
+			console.log(data);
+		});
+	};
+
+	socket.on('connect', function() {
+		$('#message-btn').click(function(e) {
+			e.preventDefault();
+			sendMsg();
+		});
+
+		$('#message-btn').keydown(function(e) {
+			var event = e ? e : window.event;
+
+			if (event.keyCode == 13) {
+				sendMsg();
+			}
+		});
+
+		socket.on('message', function(msg) {
+			var container = $('<div></div>').attr('id', 'container');
+			var bubble = $('<div></div>').addClass('talk-bubble tri-right left-in border');
+			var textWrapper = $('<div></div>').addClass('talktext').appendTo(bubble);
+			$('<p></p>').html(msg.msg).appendTo(textWrapper);
+
+			// append the jquery clone to target destination.
+			var nameClone = username2jqy(msg.username);
+			var avaClone = avatar2jqy(msg.avatar);
+
+			avaClone.appendTo(container);
+			nameClone.appendTo(container);
+			bubble.clone().appendTo(container);
+			container.clone().appendTo($('#messages'));
+
+			$('<br>').appendTo($('#messages'));
+
+			// self-adjust scrolling height.
+			var msgWrapper = document.getElementById('messages');
+			msgWrapper.scrollTop = msgWrapper.scrollHeight;
+		});
+
+		socket.on('newUser', function(msg){
+			$('#greeting').empty();
+			$('<div></div>').addClass('headline').text(msg.greeting).appendTo($('#greeting'));
+		});
+
+		socket.on('userlist', function(userlist){
+			$('#userlist').empty();
+			$('<h2></h2>').text('Online users:').appendTo($('#userlist'));
+			Object.keys(userlist).map(function(d){
+				console.log(userlist[d]);
+				var usrImg = $('<img></img>').attr('src', userlist[d].avatar);
+				var usrName = $('<div></div>').text(userlist[d].username).addClass('listname');
+				var profileWrapper = $('<div></div>').addClass('userlistProfile');
+				usrImg.appendTo(profileWrapper);
+				usrName.appendTo(profileWrapper);
+				profileWrapper.appendTo($('#userlist'));
+			});
+		});
 	});
 });
 
--- server.js
+++ server.js
@@ -19,19 +19,22 @@ io = socketIO(server);
 var sockets = [];
 var ID2user = {};
 
-
+var addNewUser = function(socket, name) {
+  console.log(socket.id);
+  var personStr = sockets.length == 1 ? 'person' : 'people';
+  socket.emit('newUser', {
+    greeting: name + " joined the chat! " + sockets.length + ' ' + personStr + " online now!"
+  });
+};
 
 io.on('connection', function(socket) {
   sockets.push(socket);
 
-  var newUser = function(socket) {
-    var personStr = sockets.length == 1 ? 'person' : 'people';
-    socket.emit('newUser', {
-      greeting: " joined the chat! " + sockets.length + ' ' + personStr + " online now!"
-    });
-  };
-
-  newUser(socket);
+  socket.on('createUser', function(usr) {
+    for(var j=0; j < sockets.length; j++){
+      addNewUser(sockets[j], usr.username);
+    }
+  });
 
   socket.on('message', function(message) {
     var userChanged = false;
@@ -39,21 +42,47 @@ io.on('connection', function(socket) {
     if (!ID2user[socket.id]) {
       var newUser = {
         'username': message.username,
-        'avatar': message.avatarSrc
+        'avatar': message.avatar
       };
+
+      console.log('newwwww');
 
       userChanged = true;
       ID2user[socket.id] = newUser;
     }
 
+    console.log(ID2user[socket.id]);
+
+    if (userChanged) console.log(ID2user[socket.id].username + '(id: ' + socket.id + ' )' + 'joins!');
+
     for (var i = 0; i < sockets.length; i++) {
       sockets[i].emit('message', message);
+
       if (userChanged) {
-        console.log(ID2user[socket.id].username + '(id: ' + socket.id + ' )' + 'joins!');
-        socket.emit('userList', ID2user);
-        newUser(sockets[i]);
+        
+        socket.emit('userlist', ID2user);
+        //addNewUser(sockets[i]);
       }
     }
+  });
+
+  socket.on('disconnect', function() {
+    var usernameOut = ID2user[socket.id].username;
+
+    for(var i=0; i<sockets.length; i++){
+      if(sockets[i].id === socket.id){
+        sockets.splice(i, 1);
+      }
+    }
+
+    delete ID2user[socket.id]; // remove user from online users.
+    // send to client an updated userlist.
+    for(var j=0; j < sockets.length; j++){
+      console.log(usernameOut + '(id: ' + socket.id + ' )' + 'leaves...');
+      sockets[j].emit('userlist', ID2user);
+      //addNewUser(sockets[j]);
+    }
+    console.log("There are " + sockets.length + " active sockets remaining.");
   });
 });
 
